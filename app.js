a8aef1f8bf07a249213efdff31aa1c56f9d6e22e
Abijot Gupta <abijotgupta1999@gmail.com>
2021-04-24 23:39:15 +0530

    Restructure the App

--- app.js
+++ app.js
@@ -1,30 +1,15 @@
 const express = require('express');
+const middleware = require('./middlewares');
+const routes = require('./routes/routeList.js');
+
+
 const app = express();
 
-
 //middleware
-
-
-
-
-
+middleware(app);
 
 //routes
-app.get('/', (req, res, next) => {
-    //res.send('Welcome to the HOMEPAGE');
-
-    // res.json( {
-    //     "message": "Welcome to the Homepage!!!"
-    // });
-
-    res.redirect('/user');
-});
-
-app.get('/user/:id/:postid', (req, res, next) => {
-    console.log(req.query)
-    console.log(req.params);
-    res.send('on the User page');
-});
+routes(app);
 
 
 module.exports = app;
