41a679bfef9611829b2d1dfb4187d58d494099ca
Abijot Gupta <abijotgupta1999@gmail.com>
2021-04-25 01:28:30 +0530

    MongoDb configured

---
+++ configuration/db.js
@@ -0,0 +1,14 @@
+const { MongoClient } = require('mongodb');
+const _uri = "mongodb+srv://RamGupta:Ramgupta@123@movieappproject.vzkl4.mongodb.net/sample_mflix?retryWrites=true&w=majority";
+
+const dbConnection = (collection, callback) => {
+    MongoClient.connect(_uri)
+    .then(async client => {
+        const db = client.db('sample_mflix').collection(collection);
+        await callback(db);
+        client.close();
+    })
+    .catch();
+}
+
+module.exports = dbConnection
